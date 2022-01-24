80fef60dfaab8c1559868e1381c30fb979dfb6ca
Robin <43524021+robinbayer@users.noreply.github.com>
2022-01-23 19:49:55 -0500

    Initial file commit

---
+++ Enums.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TequaCreek.BloxStoxDataModelLibrary
+{
+
+    /// <summary>
+    /// Defines the current status of a Game Player
+    /// </summary>
+    public enum PlayerStatus
+    {
+        Active = 1,
+        DepletedBalanceTimeout,
+        Suspended,
+        Inactive
+    }
+
+    /// <summary>
+    /// Defines the Transaction Type for a Credit or Debit Transaction
+    /// </summary>
+    public enum TransactionType
+    {
+        InitialBalanceAward = 1,
+        BuyTransaction,
+        SellTransaction,
+        ProceedsFromSale,
+        WithdrawlForPurchase,
+        CommissionFeeEarnings,
+        WithdrawlForCommissionFee,
+        DividendPayment,
+        RejoinBalanceAward,
+        OutlayForBalanceAward
+    }
+
+    /// <summary>
+    /// Defines the Asset Type for an Asset Account
+    /// </summary>
+    public enum AssetType
+    {
+        Cash = 1,
+        Stock,
+        CorporateEarnings
+    }
+
+    /// <summary>
+    /// Defines the Payload Type for a received message from In-Game Endpoint
+    /// </summary>
+    public enum FromInGamePayloadType
+    {
+        SearchGamePlayer = 1,
+        AddGamePlayer,
+        SearchStockBySymbol,
+        SearchStockByKeyword,
+        GetCurrentOwnedAssets,
+        GetActivityListForPlayer,
+        BuyAsset,
+        SellAsset
+    }
+
+    /// <summary>
+    /// Defines the Payload Type for a sent message to In-Game Endpoint
+    /// </summary>
+    public enum ToInGamePayloadType
+    {
+        _NOTYETKNOWN = 1
+    }
+
+
+}
---
+++ SharedConstantValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TequaCreek.BloxStoxDataModelLibrary
+{
+    public class SharedConstantValues
+    {
+
+        //////////////////////////////////////////////////////////////////////
+        // Shared constants between web application and processing service ///
+        //////////////////////////////////////////////////////////////////////
+
+
+    }
+}
