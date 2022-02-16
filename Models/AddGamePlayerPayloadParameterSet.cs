bb57eabc2c4fa7abfe834c2346acd718e5b326a5
Robin <43524021+robinbayer@users.noreply.github.com>
2022-02-16 16:59:24 -0500

    Create AddGamePlayerPayloadParameterSet.cs
    Added parameter set for AddGamePlayer

---
+++ Models/AddGamePlayerPayloadParameterSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TequaCreek.BloxStoxDataModelLibrary.Models
+{
+    public class AddGamePlayerPayloadParameterSet
+    {
+        public string gamePlayerExternalId { get; set; }
+        public string platformId { get; set; }
+    }
+}
